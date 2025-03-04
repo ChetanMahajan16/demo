d383ba95517f2a8e2a41f809ad51e2ad6f46c9a4
chetan <cmahajan0610@gmail.com>
2025-03-04 16:55:35 +0530

    C:/Program Files/Git/about page

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const port = 5000;
 tsc.get('/',(req,res)=> {
     res.send('hello Akrypt');
 })
+
+tes.get('/About',(req,res)=>{
+    res.send('welcome to about');
+})
  tsc.listen(port,()=>{
     console.log(`server is runing on port ${port}`);
  })
